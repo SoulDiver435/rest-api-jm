4ce2fc16ad00ebaa8286360e31842aaae996fa57
souldiver435 <juanmapv387@gmail.com>
2025-06-14 13:39:36 -0500

    Add changes 2

--- schemas/movies.js
+++ schemas/movies.js
@@ -1,4 +1,4 @@
-const z = require('zod')
+import z from 'zod'
 
 const movieSchema = z.object({
   title: z.string({
@@ -22,20 +22,15 @@ const movieSchema = z.object({
   )
 })
 
-function validateMovie (object) {
+export function validateMovie (object) {
   return movieSchema.safeParse(object)
 }
 
-function validatePartialMovie (object) {
+export function validatePartialMovie (object) {
   return movieSchema.partial().safeParse(object)
 }
 
-module.exports = {
-  validateMovie,
-  validatePartialMovie
-}
-
-// {
-//   "title": 1,
-//   "year": "sdfds3244324324"
+// module.exports = {
+//   validateMovie,
+//   validatePartialMovie
 // }
